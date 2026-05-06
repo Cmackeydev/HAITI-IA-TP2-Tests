2ebd96f685f7dd51e2b4a81e6a40d22bdf3efac2
Mackey CHARLES <cmackey016@yahoo.fr>
2026-05-06 19:56:43 -0400

    Creation du test3

---
+++ src/main/java/ht/mbds/charles/Test3.java
@@ -0,0 +1,29 @@
+package ht.mbds.charles;
+
+import java.util.Map;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String llmKey = System.getenv("GEMINI_KEY");
+    if (llmKey == null) {
+      System.out.println("La variable d'environnement GEMINI_KEY n'est pas définie.");
+      return;
+    }
+
+    ChatModel modele = GoogleAiGeminiChatModel.builder()
+.apiKey(llmKey)
+.modelName("gemini-3-flash-preview")
+.build();
+
+PromptTemplate traducteur = PromptTemplate.from("Traduire le texte suivant en anglais : {texte}");
+Prompt promptTraduction = traducteur.apply(Map.of("texte","Bonjour"));
+String reponse =modele.chat(promptTraduction.toUserMessage()).toString();
+// Affiche la réponse du modèle (hello)
+System.out.println(reponse);
+}
+}

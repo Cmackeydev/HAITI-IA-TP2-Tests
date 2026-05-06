e5ab1ec742df037eb5826aaf46a4e119d6b4a887
Mackey CHARLES <cmackey016@yahoo.fr>
2026-05-06 18:13:24 -0400

    resolution de problemes de dependances dans le jar

--- src/main/java/ht/mbds/charles/Test1.java
+++ src/main/java/ht/mbds/charles/Test1.java
@@ -1,5 +1,8 @@
 package ht.mbds.charles;
 
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+
 public class Test1 {
     public static void main(String[] args) {
         String llmKey = System.getenv("GEMINI_KEY");
@@ -7,6 +10,15 @@ public class Test1 {
       System.out.println("La variable d'environnement GEMINI_KEY n'est pas définie.");
       return;
     }
-    
+
+    ChatModel modele = GoogleAiGeminiChatModel.builder()
+.apiKey(llmKey)
+.modelName("gemini-3-flash-preview")
+.build();
+// Pose une question au modèle
+String reponse =
+modele.chat("Traduis 'bonjour' en anglais");
+// Affiche la réponse du modèle (hello)
+System.out.println(reponse);
 }
 }
